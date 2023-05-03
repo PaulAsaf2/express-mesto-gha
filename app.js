7ad22215e0eef6e5f8b2d02704a7f70691a81582
Pavel Asafov <pavelasaf@yandex.ru>
2023-05-03 21:14:13 +0600

    настроил: Подключитесь к Mongo

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const { PORT = 3000 } = process.env
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const { PORT = 3000 } = process.env;
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
-})
+});
