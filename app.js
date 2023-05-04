9e3901c4b4b9fbeb6455f389bb27328dd340498e
Pavel Asafov <pavelasaf@yandex.ru>
2023-05-04 20:14:52 +0600

    настроил: Создайте тестового пользователя

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 const { PORT = 3000 } = process.env;
-const app = express();
+const router = require('./routes/users')
 
+const app = express();
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/users', router)
+app.use('/users/:userId', router)
+
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
