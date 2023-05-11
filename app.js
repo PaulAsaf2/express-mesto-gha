144c6873fb9c08c27edadd182c5b6c6b2fb1a7dd
Pavel Asafov <pavelasaf@yandex.ru>
2023-05-11 23:06:29 +0600

    Настроил eslint

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const process = require('process');
 const { NO_DATA_FOUND } = require('./utils/constants');
-const { PORT = 3000 } = process.env;
-
 const routerUser = require('./routes/users');
 const routerCard = require('./routes/card');
+
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
@@ -14,7 +14,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use((req, res, next) => {
   req.user = {
-    _id: "6457c58c35d9935766858ce4"
+    _id: '6457c58c35d9935766858ce4',
   };
 
   next();
@@ -26,10 +26,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/users', routerUser);
 app.use('/cards', routerCard);
 
-app.use((req, res, next) => {
-  res.status(NO_DATA_FOUND).json({message: "Страница не найдена"});
-})
-
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+app.use((req, res) => {
+  res.status(NO_DATA_FOUND).json({ message: 'Страница не найдена' });
 });
+
+app.listen(PORT);
