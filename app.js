5223485b0b0ed92cdf99c49ac7f79108d2044d7b
Pavel Asafov <pavelasaf@yandex.ru>
2023-05-20 21:49:24 +0600

    Настройка ошибок доступа

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const bodyParser = require('body-parser');
 const process = require('process');
 const rateLimit = require('express-rate-limit');
 const cookieParser = require('cookie-parser');
-const { NO_DATA_FOUND } = require('./utils/constants');
+// const { NO_DATA_FOUND } = require('./utils/constants');
 const routerUser = require('./routes/users');
 const routerCard = require('./routes/card');
 const routerEnter = require('./routes/enter');
-const { auth } = require('./middlewares/auth');
+// const { auth } = require('./middlewares/auth');
 const handleError = require('./errors/handleError');
 
 const { PORT = 3000 } = process.env;
@@ -34,15 +34,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', routerEnter);
 
-app.use('/users', auth, routerUser);
-app.use('/cards', auth, routerCard);
+app.use('/users', routerUser);
+app.use('/cards', routerCard);
+// app.use('/users', auth, routerUser);
+// app.use('/cards', auth, routerCard);
 
-app.use((req, res) => {
-  res.status(NO_DATA_FOUND).json({ message: 'Страница не найдена' });
-});
+// app.use((req, res) => {
+//   res.status(NO_DATA_FOUND).json({ message: 'Страница не найдена' });
+// });
 
 app.listen(PORT);
 
 app.use(errors());
-
 app.use(handleError);
