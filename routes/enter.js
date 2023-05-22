0c819d580d4b469828ac4b6ad7a1b74c0005b655
Pavel Asafov <pavelasaf@yandex.ru>
2023-05-22 17:27:32 +0600

    ref: signup route

--- routes/enter.js
+++ routes/enter.js
@@ -2,18 +2,11 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const { createUser, login } = require('../controllers/users');
+const { signupValidation } = require('../middlewares/validation');
 
 router.post(
   '/signup',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().pattern(/(https?:\/\/)(w{3}\.)?\w+[-.~:/?#[\]@!$&'()*+,;=]*#?/),
-    }),
-  }),
+  signupValidation,
   createUser,
 );
 router.post('/signin', celebrate({
--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 module.exports.INCORRECT_DATA = 400;
 module.exports.UNAUTHORIZED = 401;
 module.exports.FORBIDDEN = 403;
@@ -6,3 +7,5 @@ module.exports.CONFLICT = 409;
 module.exports.SERVER_ERROR = 500;
 
 module.exports.mongoDBPath = 'mongodb://127.0.0.1:27017/mestodb';
+
+module.exports.checkURL = /(https?:\/\/)(w{3}\.)?\w+[-.~:/?#[\]@!$&'()*+,;=]*#?/;
